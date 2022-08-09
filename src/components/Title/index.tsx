1493a5734bc3271213953274efba1044d09ac6a8
cotton <bmtosss@gmail.com>
2022-08-09 14:55:13 +0900

    title component 적용

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './index.module.scss';
+
+const Title = (): React.ReactElement => {
+  return <h1 className={style.title}>practice Next</h1>;
+};
+
+export default Title;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next';
+import Title from 'src/components/Title';
 
 const Home: NextPage = () => {
   const hi = (x: number): number => x + 1;
-  return <>Let's practice Next.js</>;
+  return <Title></Title>;
 };
 
 export default Home;
