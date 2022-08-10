b4f1dd212a366a00841a3c067dad0f68183c0624
cotton <bmtosss@gmail.com>
2022-08-10 21:26:37 +0900

    이미지 width height 고정

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -4,7 +4,15 @@ import Image from 'next/image';
 import desertImage from 'public/image/desert.jpeg';
 
 const Content = (): React.ReactElement => {
-  return <Image src={desertImage} alt="picture of desert" />;
+  return (
+    <Image
+      src={desertImage}
+      alt="picture of desert"
+      width={300}
+      height={300}
+      layout={'fixed'}
+    />
+  );
 };
 
 export default Content;
