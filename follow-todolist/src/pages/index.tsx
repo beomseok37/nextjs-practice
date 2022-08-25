f95c8bf187001e4a974bc07c332321ca4048ffe1
cotton <bmtosss@gmail.com>
2022-08-25 15:44:10 +0900

    refactor: 이모션 마이그레이션

--- follow-todolist/src/pages/index.tsx
+++ follow-todolist/src/pages/index.tsx
@@ -2,31 +2,25 @@ import type { NextPage } from 'next';
 import Router from 'next/router';
 
 import MyButton from 'src/components/MyButton';
-import styles from 'src/styles/pages/index.module.scss';
+import { ButtonsWrapper } from 'src/styles/pages';
 import Header from '../components/header/index';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header pageName="home" />
-      <div className={styles.wrapper}>
-        <div className={styles.buttons_wrapper}>
-          <MyButton
-            size="large"
-            color="gray"
-            onClick={() => Router.push('/button')}
-          >
-            Button
-          </MyButton>
-          <MyButton
-            size="large"
-            color="gray"
-            onClick={() => Router.push('/box')}
-          >
-            Box
-          </MyButton>
-        </div>
-      </div>
+      <ButtonsWrapper>
+        <MyButton
+          size="large"
+          color="gray"
+          onClick={() => Router.push('/button')}
+        >
+          Button
+        </MyButton>
+        <MyButton size="large" color="gray" onClick={() => Router.push('/box')}>
+          Box
+        </MyButton>
+      </ButtonsWrapper>
     </>
   );
 };
