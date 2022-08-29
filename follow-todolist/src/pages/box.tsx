c025811008532c31ffc415dc12316947ab84fece
cotton <bmtosss@gmail.com>
2022-08-29 15:07:29 +0900

    chore: page에 sidebar 추가

--- follow-todolist/src/pages/box.tsx
+++ follow-todolist/src/pages/box.tsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react';
 import Box from 'src/components/Box';
 import CheckBox from 'src/components/CheckBox';
 import Header from 'src/components/Header';
+import Row from 'src/components/Grid/Row';
+import Column from 'src/components/Grid/Column';
+import SideBar from 'src/components/SideBar';
 
 import { Check } from 'src/styles/pages/box';
 
@@ -13,17 +16,20 @@ const BoxPage: NextPage = () => {
     setCheck(e.currentTarget.checked);
   };
   return (
-    <>
-      <Header pageName="home" />
-      <Box />
-      <CheckBox onChange={handleChange} checked={check}>
-        다음 약관에 모두 동의
-      </CheckBox>
-      <Check>
-        check:
-        {check ? 'true' : 'false'}
-      </Check>
-    </>
+    <Row>
+      <SideBar />
+      <Column>
+        <Header pageName="box" />
+        <Box />
+        <CheckBox onChange={handleChange} checked={check}>
+          다음 약관에 모두 동의
+        </CheckBox>
+        <Check>
+          check:
+          {check ? 'true' : 'false'}
+        </Check>
+      </Column>
+    </Row>
   );
 };
 
--- follow-todolist/src/pages/button.tsx
+++ follow-todolist/src/pages/button.tsx
@@ -3,74 +3,67 @@ import { NextPage } from 'next';
 import MyButton from 'src/components/MyButton';
 import AppBlock from 'src/components/AppBlock';
 import Header from 'src/components/Header';
+import Row from 'src/components/Grid/Row';
+import Column from 'src/components/Grid/Column';
+import SideBar from 'src/components/SideBar';
 
 import { ButtonGroup } from 'src/styles/pages/button';
 
 const ButtonPage: NextPage = () => {
   return (
-    <>
-      <Header pageName="home" />
-      <AppBlock>
-        <ButtonGroup>
-          <MyButton
-            size="large"
-            onClick={() => {
-              console.log('hihihihi');
-            }}
-          >
-            Button
-          </MyButton>
-          <MyButton
-            onMouseMove={() => {
-              console.log('mouse move');
-            }}
-          >
-            Button
-          </MyButton>
-          <MyButton size="small">Button</MyButton>
-        </ButtonGroup>
-        <ButtonGroup>
-          <MyButton size="large" color="gray">
-            Button
-          </MyButton>
-          <MyButton color="gray">Button</MyButton>
-          <MyButton size="small" color="gray">
-            Button
-          </MyButton>
-        </ButtonGroup>
-        <ButtonGroup>
-          <MyButton size="large" color="pink">
-            Button
-          </MyButton>
-          <MyButton color="pink">Button</MyButton>
-          <MyButton size="small" color="pink">
-            Button
-          </MyButton>
-        </ButtonGroup>
-        <ButtonGroup>
-          <MyButton size="large" color="pink" outline>
-            Button
-          </MyButton>
-          <MyButton color="pink" outline>
-            Button
-          </MyButton>
-          <MyButton size="small" color="pink" outline>
-            Button
-          </MyButton>
-        </ButtonGroup>
-        <ButtonGroup>
-          <MyButton size="large" fullWidth>
-            Button
-          </MyButton>
-          <MyButton color="gray" fullWidth>
-            Button
-          </MyButton>
-          <MyButton size="small" color="pink" fullWidth>
-            Button
-          </MyButton>
-        </ButtonGroup>
-      </AppBlock>
-    </>
+    <Row>
+      <SideBar />
+      <Column>
+        <Header pageName="button" />
+        <AppBlock>
+          <ButtonGroup>
+            <MyButton size="large">Button</MyButton>
+            <MyButton>Button</MyButton>
+            <MyButton size="small">Button</MyButton>
+          </ButtonGroup>
+          <ButtonGroup>
+            <MyButton size="large" color="gray">
+              Button
+            </MyButton>
+            <MyButton color="gray">Button</MyButton>
+            <MyButton size="small" color="gray">
+              Button
+            </MyButton>
+          </ButtonGroup>
+          <ButtonGroup>
+            <MyButton size="large" color="pink">
+              Button
+            </MyButton>
+            <MyButton color="pink">Button</MyButton>
+            <MyButton size="small" color="pink">
+              Button
+            </MyButton>
+          </ButtonGroup>
+          <ButtonGroup>
+            <MyButton size="large" color="pink" outline>
+              Button
+            </MyButton>
+            <MyButton color="pink" outline>
+              Button
+            </MyButton>
+            <MyButton size="small" color="pink" outline>
+              Button
+            </MyButton>
+          </ButtonGroup>
+          <ButtonGroup>
+            <MyButton size="large" fullWidth>
+              Button
+            </MyButton>
+            <MyButton color="gray" fullWidth>
+              Button
+            </MyButton>
+            <MyButton size="small" color="pink" fullWidth>
+              Button
+            </MyButton>
+          </ButtonGroup>
+        </AppBlock>
+      </Column>
+    </Row>
   );
 };
 export default ButtonPage;
