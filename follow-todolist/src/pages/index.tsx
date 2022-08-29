3069d03367cb29e1cfd9292f6d7fa93541c38d8a
cotton <bmtosss@gmail.com>
2022-08-29 15:11:10 +0900

    chore: 홈페이지 수정

--- follow-todolist/src/pages/index.tsx
+++ follow-todolist/src/pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next';
-import Router from 'next/router';
 
-import MyButton from 'src/components/MyButton';
 import Header from 'src/components/Header';
 import SideBar from 'src/components/SideBar';
 import Row from 'src/components/Grid/Row';
 import Column from 'src/components/Grid/Column';
-
-import { ButtonsWrapper } from 'src/styles/pages';
 
 const Home: NextPage = () => {
   return (
@@ -15,22 +11,7 @@ const Home: NextPage = () => {
       <SideBar />
       <Column>
         <Header pageName="home" />
-        <ButtonsWrapper>
-          <MyButton
-            size="large"
-            color="gray"
-            onClick={() => Router.push('/button')}
-          >
-            Button
-          </MyButton>
-          <MyButton
-            size="large"
-            color="gray"
-            onClick={() => Router.push('/box')}
-          >
-            Box
-          </MyButton>
-        </ButtonsWrapper>
+        <p>이 블로그는 frontend의 여러 기술들을 연습하기 위한 사이트이다.</p>
       </Column>
     </Row>
   );
