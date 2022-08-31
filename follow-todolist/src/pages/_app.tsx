720a4b64ce70a6a76b7a6092a9c5c6ee2a0ca439
cotton <bmtosss@gmail.com>
2022-08-31 11:17:50 +0900

    feat: counter redux 설정
    - store 생성
    - counterSlice 생성
    - rootReducer 생성

--- follow-todolist/src/pages/_app.tsx
+++ follow-todolist/src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import { Global, ThemeProvider } from '@emotion/react';
+
+import store from 'src/redux/store';
 
 import { global } from 'src/styles/global';
 import { theme } from 'src/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={global} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
