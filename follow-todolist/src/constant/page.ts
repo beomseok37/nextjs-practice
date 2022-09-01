ce351fb5c4ad40ebef2e3e4876c8e8356c2bdc2d
cotton <bmtosss@gmail.com>
2022-09-01 09:52:25 +0900

    chore: react-hooks 페이지 상수 선언
    - page content 상수

--- follow-todolist/src/constant/page.ts
+++ follow-todolist/src/constant/page.ts
@@ -3,4 +3,11 @@ const BOX_PAGE_CONTENT = `todolist 상태관리 하는 법 (벨로퍼트님의 �
 const BUTTON_PAGE_CONTENT = `컴포넌트 만들기 기초, component의 rest props, styled in
 js(emotion), mixin 연습`;
 const REDUX_PAGE_CONTENT = `redux를 이용해 counter 만들기`;
-export { BOX_PAGE_CONTENT, BUTTON_PAGE_CONTENT, REDUX_PAGE_CONTENT };
+const REACT_HOOKS_PAGE_CONTENT = `react-hooks인 useMemo와 useCallback을 공부하는 페이지입니다.`;
+
+export {
+  BOX_PAGE_CONTENT,
+  BUTTON_PAGE_CONTENT,
+  REDUX_PAGE_CONTENT,
+  REACT_HOOKS_PAGE_CONTENT,
+};
