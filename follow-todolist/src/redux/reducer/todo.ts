dd029f34168fd5a764aad74a9828195adbdba827
cotton <bmtosss@gmail.com>
2022-09-04 22:06:14 +0900

    chore: todo list reducer 구조 생성

---
+++ follow-todolist/src/redux/reducer/todo.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+// import type { PayloadAction } from '@reduxjs/toolkit';
+// import type { RootState } from 'src/redux/store';
+
+// interface TodoListState {
+//   value: number;
+// }
+
+// const initialState: TodoListState = {
+//   value: 0,
+// };
+
+export const counterSlice = createSlice({
+  name: 'todoList',
+  //   initialState,
+  //   reducers: {
+  //     increment: (state) => {
+  //       state.value += 1;
+  //     },
+  //     decrement: (state) => {
+  //       state.value -= 1;
+  //     },
+  //     incrementByAmount: (state, action: PayloadAction<number>) => {
+  //       state.value += action.payload;
+  //     },
+  //   },
+});
+
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// export const selectCount = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer;
