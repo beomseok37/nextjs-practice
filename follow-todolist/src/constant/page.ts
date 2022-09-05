3931fd1bc2ad6136005f9b9335788629f83bcb31
cotton <bmtosss@gmail.com>
2022-09-05 14:36:34 +0900

    chore: 상수 추가

--- follow-todolist/src/constant/page.ts
+++ follow-todolist/src/constant/page.ts
@@ -2,12 +2,14 @@ const BOX_PAGE_CONTENT = `todolist 상태관리 하는 법 (벨로퍼트님의 �
     예정)`;
 const BUTTON_PAGE_CONTENT = `컴포넌트 만들기 기초, component의 rest props, styled in
 js(emotion), mixin 연습`;
-const REDUX_PAGE_CONTENT = `redux를 이용해 counter 만들기`;
+const REDUX_COUNT_CONTENT = `redux를 이용해 counter 만들기`;
+const REDUX_TODO_LIST_CONTENT = `redux를 이용해 todo list 만들기`;
 const REACT_HOOKS_PAGE_CONTENT = `react-hooks인 useMemo와 useCallback을 공부하는 페이지입니다.`;
 
 export {
   BOX_PAGE_CONTENT,
   BUTTON_PAGE_CONTENT,
-  REDUX_PAGE_CONTENT,
+  REDUX_COUNT_CONTENT,
+  REDUX_TODO_LIST_CONTENT,
   REACT_HOOKS_PAGE_CONTENT,
 };
