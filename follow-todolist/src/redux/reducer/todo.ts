8dedbc78c2f052fe1148d4945ffae2547daf4e72
cotton <bmtosss@gmail.com>
2022-09-05 14:37:26 +0900

    chore: todo list initialstate 수정
    - initialstate를 객체 형식으로 수정

--- follow-todolist/src/redux/reducer/todo.ts
+++ follow-todolist/src/redux/reducer/todo.ts
@@ -4,25 +4,37 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from 'src/redux/store';
 import TodoType from 'src/types/toDo';
 
-const initialState: TodoType[] = [];
+interface StateType {
+  todoList: TodoType[];
+}
+
+const initialState: StateType = { todoList: [] };
 
 export const todoListSlice = createSlice({
   name: 'todoList',
   initialState,
   reducers: {
     add: (state, action: PayloadAction<TodoType>) => {
-      state.push(action.payload);
+      state.todoList.push(action.payload);
     },
-    remove: (state, action: PayloadAction<TodoType>) => {
-      state = state.filter((todo) => {
-        return todo.work !== action.payload.work;
+    remove: (state, action: PayloadAction<string>) => {
+      state.todoList = state.todoList.filter(
+        (todo) => todo.work !== action.payload
+      );
+    },
+    check: (state, action: PayloadAction<TodoType>) => {
+      state.todoList = state.todoList.map((todo) => {
+        if (todo.work === action.payload.work) {
+          todo.check = !todo.check;
+        }
+        return todo;
       });
     },
   },
 });
 
-export const { add, remove } = todoListSlice.actions;
+export const { add, remove, check } = todoListSlice.actions;
 
-export const selectTodoList = (state: RootState) => state.todoList;
+export const selectTodoList = (state: RootState) => state.todoList.todoList;
 
 export default todoListSlice.reducer;
