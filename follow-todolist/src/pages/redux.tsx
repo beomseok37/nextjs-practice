965c0051f4fcde066e6572f903b7ef3ae8016052
cotton <bmtosss@gmail.com>
2022-09-05 16:03:43 +0900

    chore: pageContent 수정

--- follow-todolist/src/pages/redux.tsx
+++ follow-todolist/src/pages/redux.tsx
@@ -21,7 +21,7 @@ const Redux: NextPage = () => {
         <Header pageName="Redux" />
         <p>이 페이지는 Redux를 공부하기 위한 페이지이다.</p>
         <PageContent content={REDUX_COUNT_CONTENT} done />
-        <PageContent content={REDUX_TODO_LIST_CONTENT} />
+        <PageContent content={REDUX_TODO_LIST_CONTENT} done />
         <ReduxCounter />
         <ReduxTodoList />
       </Column>
