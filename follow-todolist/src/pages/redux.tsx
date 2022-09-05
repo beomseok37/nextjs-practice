f2226c1540f97cc40ed373768fbcc6a05cd0abbd
cotton <bmtosss@gmail.com>
2022-09-05 14:37:45 +0900

    chore: PageContent 추가

--- follow-todolist/src/pages/redux.tsx
+++ follow-todolist/src/pages/redux.tsx
@@ -8,7 +8,10 @@ import ReduxCounter from 'src/components/ReduxCounter';
 import PageContent from 'src/components/PageContent';
 import ReduxTodoList from 'src/components/ReduxTodoList';
 
-import { REDUX_PAGE_CONTENT } from 'src/constant/page';
+import {
+  REDUX_COUNT_CONTENT,
+  REDUX_TODO_LIST_CONTENT,
+} from 'src/constant/page';
 
 const Redux: NextPage = () => {
   return (
@@ -17,7 +20,8 @@ const Redux: NextPage = () => {
       <Column width="100%">
         <Header pageName="Redux" />
         <p>이 페이지는 Redux를 공부하기 위한 페이지이다.</p>
-        <PageContent content={REDUX_PAGE_CONTENT} />
+        <PageContent content={REDUX_COUNT_CONTENT} done />
+        <PageContent content={REDUX_TODO_LIST_CONTENT} />
         <ReduxCounter />
         <ReduxTodoList />
       </Column>
