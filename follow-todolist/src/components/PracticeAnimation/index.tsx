ebdb8cb061f76e9de6988fb5a85951522ff6ec0c
cotton <bmtosss@gmail.com>
2022-09-07 14:24:40 +0900

    feat: Rotate css 실험을 위한 큐브 모형 생성

--- follow-todolist/src/components/PracticeAnimation/index.tsx
+++ follow-todolist/src/components/PracticeAnimation/index.tsx
@@ -3,18 +3,39 @@ import { ReactElement, useState } from 'react';
 import Column from 'src/components/Grid/Column';
 import Button from 'src/components/base/Button';
 
-import { MoveBox } from './style';
+import { MoveBox, Rotate, Scene } from './style';
 
 function PracticeAnimation(): ReactElement {
   const [state, setState] = useState(0);
   const handleClick = () => {
-    setState(state > 0 ? state - 80 : state + 80);
+    setState(state + 90);
   };
   return (
     <Column>
       <p>animation</p>
       <Button onClick={handleClick}>click</Button>
-      <MoveBox state={state}>hihi</MoveBox>
+      <Scene>
+        <Rotate>
+          <MoveBox rotateY="0" color="red">
+            front
+          </MoveBox>
+          <MoveBox rotateY="90" color="blue">
+            Right
+          </MoveBox>
+          <MoveBox rotateY="180" color="purple">
+            Back
+          </MoveBox>
+          <MoveBox rotateY="-90" color="green">
+            Left
+          </MoveBox>
+          <MoveBox rotateX="90" color="orange">
+            Left
+          </MoveBox>
+          <MoveBox rotateX="-90" color="grey">
+            Left
+          </MoveBox>
+        </Rotate>
+      </Scene>
     </Column>
   );
 }
--- follow-todolist/src/components/PracticeAnimation/style.ts
+++ follow-todolist/src/components/PracticeAnimation/style.ts
@@ -1,19 +1,40 @@
 import styled from '@emotion/styled';
 
 interface props {
-  state: number;
+  rotateY?: string;
+  rotateX?: string;
+  color: string;
 }
 
-const MoveBox = styled.div<props>`
-  width: 100px;
-  height: 100px;
-  background: red;
-  transition: all ease 0.5s;
-  ${({ state }) =>
-    `
-  transform: rotateY(${state.toString()}deg) translateX(100px);
-  `}
+const Scene = styled.div`
+  width: 300px;
+  height: 300px;
+  padding: 100px;
+  perspective: 400px;
 `;
 
-// eslint-disable-next-line import/prefer-default-export
-export { MoveBox };
+const Rotate = styled.div`
+  box-sizing: border-box;
+  transform-style: preserve-3d;
+`;
+
+const MoveBox = styled.div<props>`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  transition: all ease 0.5s;
+  ${({ rotateY }) =>
+    rotateY &&
+    `
+  transform: rotateY(${rotateY}deg) translateZ(50px)
+  `};
+  ${({ rotateX }) =>
+    rotateX && `transform: rotateX(${rotateX}deg) translateZ(50px)`};
+  background: ${({ color }) => color};
+  opacity: 0.7;
+`;
+
+export { MoveBox, Rotate, Scene };
