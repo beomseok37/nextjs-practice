8124b76644eaed9e58f737d403e4a60f101fdbf4
cotton <bmtosss@gmail.com>
2022-09-14 16:51:35 +0900

    chore: 상수 선언

--- follow-todolist/src/constant/page.ts
+++ follow-todolist/src/constant/page.ts
@@ -5,11 +5,14 @@ js(emotion), mixin 연습`;
 const REDUX_COUNT_CONTENT = `redux를 이용해 counter 만들기`;
 const REDUX_TODO_LIST_CONTENT = `redux를 이용해 todo list 만들기`;
 const REACT_HOOKS_PAGE_CONTENT = `react-hooks인 useMemo와 useCallback을 공부하는 페이지입니다.`;
-
+const ANIMIATION_PAGE_CONTENT = `transform, translate 공부`;
+const DROPDOWN_MENU_PAGE_CONTENT = `dropdown list 만들어보기`;
 export {
   BOX_PAGE_CONTENT,
   BUTTON_PAGE_CONTENT,
   REDUX_COUNT_CONTENT,
   REDUX_TODO_LIST_CONTENT,
   REACT_HOOKS_PAGE_CONTENT,
+  ANIMIATION_PAGE_CONTENT,
+  DROPDOWN_MENU_PAGE_CONTENT,
 };
